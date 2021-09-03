0047bf42f00abb7c19b3401f2b0e03008ef50a95
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-09-03 23:02:20 +0530

    Add files via upload

--- JS/EmployeeDetails.js
+++ JS/EmployeeDetails.js
@@ -1,163 +1,111 @@
-window.addEventListener('DOMContentLoader', (event) => {
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function () {
-        if (name.value.length == 0) {
-            textError.textContent = "";
-            return;
-        }
-        try {
-            (new EmployeePayrollData()).name = name.value;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
-        }
-    });
-
-    const salary = document.querySelector('#salary');
-    const output = document.querySelector('.salary-output');
-    output.textContent = salary.value;
-    salary.addEventListener('input', function () {
-        output.textContent = salary.value;
-    });
+let empPayrollList;
+/**
+ * Using Template Literals in the employee payroll details
+ */
+window.addEventListener("DOMContentLoaded", (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem("editEmp");
 });
 
-const save = () => {
-    try {
-        let employeePayrollData = createEmployeePayroll();
-        createAndUpdateStorage(employeePayrollData);
-    } catch (e) {
-        return;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+};
+
+
+function createInnerHtml(){
+    const headerHtml = "<tr><th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th>"+
+    "<th>start Date</th><th>Actions</th></tr>";
+
+    if (empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for (const empPayrollData of empPayrollList) {
+        innerHtml = `${innerHtml}
+           <tr>
+           <td><img src="${empPayrollData._profilePic}" class="profile" width="30px" alt=""></td>
+           <td>${empPayrollData._name}</td>
+           <td>${empPayrollData._gender}</td>
+           <td>${getDeptHtml(empPayrollData._department)}</td>
+           <td>${empPayrollData._salary}</td>
+           <td>${stringifyDate(empPayrollData._startDate)}</td>
+           <td>
+           <img id="${
+            empPayrollData._id
+          }" onclick="remove(this)" alt="delete" width="30px" src="../assets/assets/icons/delete-black-18dp.svg">
+          <img id="${
+            empPayrollData._id
+          }" onclick="update(this)" alt="edit" width="30px" src="../assets/assets/icons/create-black-18dp.svg ">
+           </td>
+       </tr>`
     }
+    document.querySelector('#table-display').innerHTML = innerHtml
 }
 
-function saveEmployeeDetails() {
-    let name = document.getElementById("name").value
-    console.log(name);
-
-    let department = document.getElementsByClassName("checkbox")
-    for (var i = 0; i < department.length; i++) {
-        if (department[i].checked) {
-            console.log(department[i].value)
-        }
-    }
-    console.log(department);
-
-    let gender = document.getElementsByName("gender")
-    for (var i = 0; i < gender.length; i++) {
-        if (gender[i].checked) {
-            console.log(gender[i].value)
-        }
-    }
-    console.log(gender);
-}
-
-
-// const getSelectedValues = (propertyValue) => {
-//     let allItems = document.querySelectorAll(propertyValue);
-//     let selItems = [];
-//     allItems.forEach(item => {
-//         if (item.checked) selItems.push(item.value);
-//     });
-//     return selItems;
-// }
-
-// const getInputValueById = (id) => {
-//     let value = document.querySelector(id).value;
-//     return value;
-// }
-
-// const getInputElementValue = (id) => {
-//     let value = document.getElementById(id).value;
-//     return value;
-// }
-
-function createAndUpdateStorage(employeePayrollData) {
-    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
-    if (employeePayrollList != undefined) {
-        employeePayrollList.push(employeePayrollData);
-    } else {
-        employeePayrollList = [employeePayrollData];
-    }
-    alert(employeePayrollList.toString());
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
-}
-
-const resetForm = () => {
-    setValue('#name', '');
-    unsetSelectedValues('[name=profile]');
-    unsetSelectedValues('[name=gender]');
-    unsetSelectedValues('[name = department]');
-    setValues('#salary', '');
-    setValue('#notes', '');
-    setValue('#startDate', '1-January-2020');
-}
-
-const unsetSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    allItems.forEach(item => {
-        item.checked = false;
-    });
-}
-
-const setTextValue = (id, value) => {
-    const element = document.querySelector(id);
-    element.textContent = value;
-}
-
-const setValue = (id, value) => {
-    const element = document.querySelector(id);
-    element.value = value;
-}
-
-const getDeptHtml = (deptList) => {
-    let deptHtml = '';
+function getDeptHtml(deptList) {
+    let deptHtml = "";
     for (const dept of deptList) {
-        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
     }
     return deptHtml;
 }
 
-// const createEmployeePayrollJSON = () => {
-//     let empPayrollListLocal = [
-//         {
-//             _name : 'Meghna',
-//             _gender : 'Female',
-//             _department : [
-//                 'Engineering',
-
-//             ],
-//             _salary: '1000',
-//             _startDate: '1 Jan 2021',
-//             _note: '',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/profile-images/Ellipse -3.png'
-//         },
-//         {
-//             _name : 'Mayank',
-//             _gender : 'Male',
-//             _department : [
-//                 'Finance',
-
-//             ],
-//             _salary: '2000',
-//             _startDate: '1 Feb 2021',
-//             _note: '',
-//             _id: new Date().getTime(),
-//             _profilePic: '../assets/profile-images/Ellipse -1.png'
-//         }
-//     ];
-//     return empPayrollListLocal;
-// }
-
+//UC to Remove an Employee from the Payroll details.
 const remove = (node) => {
-    let empPayrollData = empPayrollList.find(empData.id == node.id);
-    if(!empPayrollData) return;
+    let empPayrollData = empPayrollList.find((empData) => empData._id == node.id);
+    if (!empPayrollData) {
+        return;
+    }
     const index = empPayrollList
-            .map(empData => empData.id)
-            .indexOf(empPayrollData._id);
-    empPayrollList.splice(index,1);
+        .map((empData) => empData._id)
+        .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
     localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
-}
+};
+
+
+/*const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name : 'Rahul Deshpande',
+            _gender : 'Male',
+            _department : [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '500000',
+            _startDate: '9 Oct 2018',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/assets/profile-images/Ellipse -8.png'
+        },
+        {
+            _name : 'IshuKumar',
+            _gender : 'Male',
+            _department : [
+                'Engineering',
+                
+            ],
+            _salary: '500000',
+            _startDate: '29 June 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/assets/profile-images/Ellipse -9.png'
+        },
+        {
+            _name : 'Meghna Dipanikar',
+            _gender : 'Female',
+            _department : [
+                'Sales','HR'
+            ],
+            _salary: '350000',
+            _startDate: '12 June 2020',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/assets/profile-images/Ellipse -7.png'   
+        }
+    ];
+    return empPayrollListLocal;
+}*/
